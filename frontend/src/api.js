fe09fbe39f87d0e5a90a6cc5fc9bbaa6ad395e0f
Waree <wareelikhitchatchawan@gmail.com>
2026-03-18 16:11:02 +0700

    update api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://todo-app-production-f0e4.up.railway.app/api' });
 
 export const getTasks = (userId, filters = {}) =>
   api.get(`/users/${userId}/tasks`, { params: filters }).then(r => r.data);
