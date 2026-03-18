9b7774039f916b1bf587c50083ad59fdf53d1c29
Waree <wareelikhitchatchawan@gmail.com>
2026-03-18 16:55:47 +0700

    add db port

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'todo_db',
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
 });
